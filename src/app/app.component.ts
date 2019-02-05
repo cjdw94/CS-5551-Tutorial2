30bfd531b51285992a901dc297403fea8680a1dc
Dhabbah <46940721+Dhabbah@users.noreply.github.com>
2019-02-04 22:08:48 -0600

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,34 @@
 import { Component } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 @Component({
-  selector: 'my-app',
+  selector: 'app-my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
   name = 'to Lab2';
+  state = 'MO';
+  city = 'Kansas City';
   result: any;
-  link= 'https://api.wunderground.com/api/4bbbc25f4f5946dd/conditions/q/MO/kansas%20city.json';
-  link2= 'https://api.wunderground.com/api/36b799dc821d5836/forecast/q/MO/Kansas_city.json';
-  constructor(private http: HttpClient){
+  result2: any;
+  link1 = 'https://api.wunderground.com/api/4bbbc25f4f5946dd/conditions/q/MO/kansas%20city.json';
+  link2 = 'https://api.wunderground.com/api/36b799dc821d5836/hourly/q/MO/Kansas_City.json';
+  constructor(private http: HttpClient) {
   }
-  show(){
-    this.http.get(this.link)
-      .subscribe((result)=> {
-        this.result = result;
-        console.log(this.result);
-      });
-    this.http.get(this.link2)
-      .subscribe((result)=> {
+  show() {
+    this.http.get('https://api.wunderground.com/api/4bbbc25f4f5946dd/conditions/q/' + this.state + '/' + this.city + '.json')
+      .subscribe((result) => {
         this.result = result;
         console.log(this.result);
 
       });
+    this.http.get('https://api.wunderground.com/api/36b799dc821d5836/hourly/q/' + this.state + '/' + this.city + '.json')
+      .subscribe((result2) => {
+        this.result2 = result2;
+        console.log(this.result2);
+
+      });
+
   }
 }
+
